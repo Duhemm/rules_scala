67961e654335526c9044272780b3208edbd25289
Adam Singer <adams@twitter.com>
2021-03-02 00:27:45 -0800

    Modify example to NoSuchMethodError on ByteBuffer.flip()

--- examples/testing/scalatest_repositories/example/ExampleTest.scala
+++ examples/testing/scalatest_repositories/example/ExampleTest.scala
@@ -4,6 +4,10 @@ import org.scalatest.{FlatSpec, MustMatchers}
 
 class ExampleTest extends FlatSpec with MustMatchers {
   "Exmaple" should "pass" in {
+    println("test1")
+    val byteBuffer = java.nio.ByteBuffer.allocate(2)
+    byteBuffer.putChar('a')
+    byteBuffer.flip()
     1 must be(1)
   }
 }
